a73e766dd129ce33deee50bb9e72a6dc0c724af9
juanib99 <juanignaciobisello@hotmail.com>
2025-08-28 17:45:22 -0300

    +

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        message: 'Server is running'
+    })
+});
+const port = process.env.PORT || 4000;
+
+app.listen( port, () => {
+    console.log('Server running on port: ', port);
+})
