28f01cd454975317208474342b44e5d89636a5da
juanib99 <juanignaciobisello@hotmail.com>
2025-08-30 18:58:39 -0300

    configuracion typescript + node

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
     res.json({
         ok: true,
         message: 'Server is running'
